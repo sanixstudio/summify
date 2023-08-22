efbb07e3b06ee928cadd8fbdd5a69c73e246e7c5
Adnan Niaz <sanixstudio@gmail.com>
2023-08-22 00:05:45 -0700

    created client setup with tailwind and react-router

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <div>Left</div>
+      <div>Right</div>
+    </div>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root.tsx";
+import { SpinnerLoader } from "./components/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/home",
+    element: <h1>Home</h1>,
+  },
+  {
+    path: "/login",
+    element: <h1>Login</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} fallbackElement={<SpinnerLoader />} />
+  </React.StrictMode>
+);
